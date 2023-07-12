b1e2d5c93b6078fa5ae947089e625bdbb41b2477
dzhaniivanov94 <68897146+dzhaniivanov94@users.noreply.github.com>
2023-07-12 17:26:59 +0300

    added navbar,work on home page

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,4 +1,17 @@
-const HomePage = () => {
-  return <div>HomePage</div>;
+import getBillboard from "@/actions/get-billboard";
+import Billboard from "@/components/billboard";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const billboard = await getBillboard("7df1c1e4-9e1c-4fea-91d5-a5c909192010");
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+    </Container>
+  );
 };
 export default HomePage;
--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -16,6 +16,7 @@ const MainNav: React.FC<MainNavProps> = ({ data }) => {
     label: route.name,
     active: pathname === `/category/${route.id}`
   }));
+
   return (
     <nav className="mx-6 flex items-center space-x-4 lg:space-x-6">
       {routes.map(route => (
@@ -26,7 +27,9 @@ const MainNav: React.FC<MainNavProps> = ({ data }) => {
             "text-sm font-medium transition-colors hover:text-black",
             route.active ? "text-black" : "text-neutral-500"
           )}
-        ></Link>
+        >
+          {route.label}
+        </Link>
       ))}
     </nav>
   );

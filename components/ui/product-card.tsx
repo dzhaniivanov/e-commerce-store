0b957071a5a3bf92168359e10b6349bdd2a6815d
dzhaniivanov94 <68897146+dzhaniivanov94@users.noreply.github.com>
2023-07-13 10:21:02 +0300

    product list implemented

---
+++ components/ui/product-card.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { Product } from "@/types";
+import Image from "next/image";
+import IconButton from "@/components/ui/icon-button";
+import { Expand, ShoppingCart } from "lucide-react";
+import Currency from "@/components/ui/currency";
+
+interface ProductCard {
+  data: Product;
+}
+
+const ProductCard: React.FC<ProductCard> = ({ data }) => {
+  return (
+    <div className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
+      <div className="aspect-square rounded-xl bg-gray-100 relative">
+        <Image
+          className="aspect-square object-cover rounded-md"
+          width={300}
+          height={350}
+          alt="image"
+          src={data?.images?.[0]?.url}
+        />
+        <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
+          <div className="flex gap-x-6 justify-center">
+            <IconButton
+              onClick={() => {}}
+              icon={<Expand size={20} className="text-gray-600" />}
+            />
+            <IconButton
+              onClick={() => {}}
+              icon={<ShoppingCart size={20} className="text-gray-600" />}
+            />
+          </div>
+        </div>
+      </div>
+      {/* descritpion */}
+      <div>
+        <p className="font-semibold text-lg">{data.name}</p>
+        <p className="text-sm text-gray-500">{data.category?.name}</p>
+      </div>
+      {/* price */}
+      <div className="flex items-center">
+        <Currency value={data?.price} />
+      </div>
+    </div>
+  );
+};
+export default ProductCard;

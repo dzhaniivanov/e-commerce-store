333639369e8bb55d6e9c22b294a28719d86ecb71
dzhaniivanov94 <68897146+dzhaniivanov94@users.noreply.github.com>
2023-07-13 15:48:08 +0300

    product modal implemented

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 
 import Footer from "@/components/footer";
 import Navbar from "@/components/navbar";
+import ModalProvider from "@/providers/modal-provider";
 
 const font = Urbanist({ subsets: ["latin"] });
 
@@ -20,7 +21,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Navbar/>
+        <ModalProvider />
+        <Navbar />
         {children}
         <Footer />
       </body>
